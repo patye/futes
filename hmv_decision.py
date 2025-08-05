4661d72cc3775db33d7b6a2943e11d8cfde33e18
magyarpeter <peter.m@trueforexfunds.com>
2025-08-05 19:54:41 +0200

    fix: add logging

--- hmv_decision.py
+++ hmv_decision.py
@@ -1,7 +1,21 @@
+import logging
+
 class HmvDecision:
 
-
   def hmv_decision(self, hmv_on, temperature):
+      logger = logging.getLogger()
+      logger.setLevel(logging.INFO)
+
+      # define file handler and set formatter
+      formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+      file_handler = logging.FileHandler('/var/log/heating/heating.log')
+      file_handler.setFormatter(formatter)
+
+      # add file handler to logger
+      logger.addHandler(file_handler)
+
+
+      logger.info("Temperature is: " + str(temperature))
 
       global boiler
       hmv_hysteresis = {
@@ -10,8 +24,10 @@ class HmvDecision:
       }
       if hmv_on and temperature >= hmv_hysteresis["temp_high"]:
           boiler = False
+          logger.info("Boiler is: %s", boiler)
       elif hmv_on and temperature <= hmv_hysteresis["temp_low"]:
           boiler = True
+          logger.info("Boiler is: %s", boiler)
       return boiler
 
 
